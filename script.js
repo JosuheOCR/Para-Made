3a236083f745c4791db6769cf4b1d9950d75c04e
JosuheOCR <joshueocr@gmail.com>
2025-02-15 00:52:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,4 +19,31 @@ document.getElementById('siBtn').addEventListener('click', function() {
     document.getElementById('noBtn').style.display = 'none';
     document.getElementById('messageContainer').style.display = 'block';
     document.getElementById('messageContainer').innerHTML = 'Te amo Made, sabía que dirías que sí, puedes escribirme o llamarme cuando quieras, en general puedes hablarme cuando quieras, porque sin importar el tiempo que pase serás para mí tú y solo tú.';
+};
+
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+    margin: 10px;
+    cursor: pointer;
+    z-index: 10; /* Asegura que los botones estén por encima de los corazones */
+}
+
+#messageContainer {
+    font-size: 30px;
+    color: #FF0000; /* Rojo */
+    font-family: 'Arial', sans-serif;
+    text-shadow: 2px 2px 4px #000; /* Borde simulado */
+    animation: pulse 0.5s infinite alternate;
+    display: none;
+    z-index: 10; /* Asegura que el mensaje esté por encima de los corazones */
+}
+
+.heart {
+    position: absolute;
+    color: #ff0066;
+    font-size: 25px;
+    animation: floatingHearts 5s infinite ease-in;
+    opacity: 0;
+    z-index: 1. /* Asegura que los corazones estén detrás de los elementos interactivos */
 });
